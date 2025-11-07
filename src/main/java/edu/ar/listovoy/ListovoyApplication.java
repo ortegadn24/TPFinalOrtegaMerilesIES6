12db96d72b62d01c4ee879b1663fb8e7982b9d85
Daiana Nicole Ortega <ortegadn24@gmail.com>
2025-11-07 09:52:19 -0300

    Inicio del gran proyecto

---
+++ src/main/java/edu/ar/listovoy/ListovoyApplication.java
@@ -0,0 +1,13 @@
+package edu.ar.listovoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListovoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListovoyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ar/listovoy/ListovoyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ar.listovoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListovoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
