28f6b4663d5c190ac3c605383a48af26b0651331
Daiana Nicole Ortega <ortegadn24@gmail.com>
2025-11-19 02:49:39 -0300

    Modificacion y avances en Controller y HTML U-C

--- src/main/java/edu/ar/listovoy/controller/ConductorController.java
+++ src/main/java/edu/ar/listovoy/controller/ConductorController.java
@@ -2,12 +2,18 @@ package edu.ar.listovoy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import edu.ar.listovoy.model.Conductor;
+
 
 @Controller
 public class ConductorController {
 
+    Conductor nuevoConductor = new Conductor ();  //aqui creamos un objeto nuevo alumno de tipo Alumno- en un constructur vacio
+
     @GetMapping("/conductores")
-    public String listarConductores() {
-        return "conductores"; // Nombre del HTML que quieras mostrar
+    public String getConductores() {
+
+     
+        return "detalleConductor"; // Nombre del HTML que quieras mostrar
     }
 }
